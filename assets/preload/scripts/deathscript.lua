773083a936141025e0d1cbacdb8d88f2fe48ee87
Nael1820 <94629276+Nael1820@users.noreply.github.com>
2022-07-14 18:06:23 +0800

    Add files via upload

---
+++ assets/preload/scripts/deathscript.lua
@@ -0,0 +1,4 @@
+function onGameOver()
+    setPropertyFromClass('GameOverSubstate', 'characterName', 'deathrex'); --Character json file for the death animation
+    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'rexdeath'); --put in mods/sounds/
+end
